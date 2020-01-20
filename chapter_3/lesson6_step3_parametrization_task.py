83bea0523f005e9e25e9e852a9b62ce4cfe7b8ba
kazantsev-sergey <bbmm@bk.ru>
2020-01-20 16:04:12 +0700

    added chapter3 lesson 6 - parametrization step 3 with time

--- chapter_3/lesson6_step3_parametrization_task.py
+++ chapter_3/lesson6_step3_parametrization_task.py
@@ -23,18 +23,17 @@ def test_guest_should_see_login_link(browser, url):
     link = f"{url}"
     browser.get(link)
 
-    #input = WebDriverWait(browser, 15).until(EC.element_to_be_clickable(By.CSS_SELECTOR(".string-quiz__textarea")))
     time.sleep(10)
+    #input = WebDriverWait(browser, 15).until(EC.element_to_be_clickable(By.CSS_SELECTOR(".string-quiz__textarea")))
     input = browser.find_element_by_css_selector(".string-quiz__textarea")
     submit_button = browser.find_element_by_css_selector(".submit-submission")
 
     answer = math.log(int(time.time()))
-
-    browser.implicitly_wait(15)
     input.send_keys(str(answer))
     submit_button.click()
 
     time.sleep(10)
+    #hint = WebDriverWait(browser, 15).until(EC.element_to_be_clickable(By.CSS_SELECTOR(".smart-hints__hint")))
     hint = browser.find_element_by_css_selector(".smart-hints__hint")
     assert hint.text == "Correct!", "The answer is wrong"
     print(hint.text)
