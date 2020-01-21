96ed92f80d0d5451b0b3d66f19a300f5366a3569
kazantsev-sergey <bbmm@bk.ru>
2020-01-21 18:26:54 +0700

    added chapter3 lesson 6 - parametrization step 3 with time

--- chapter_3/lesson6_step3_parametrization_task.py
+++ chapter_3/lesson6_step3_parametrization_task.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
@@ -15,11 +15,18 @@ def browser():
     browser.quit()
 
 URLS = [
-    'https://stepik.org/lesson/236895/step/1'
+    'https://stepik.org/lesson/236895/step/1',
+    'https://stepik.org/lesson/236896/step/1',
+    'https://stepik.org/lesson/236897/step/1',
+    'https://stepik.org/lesson/236898/step/1',
+    'https://stepik.org/lesson/236899/step/1',
+    'https://stepik.org/lesson/236903/step/1',
+    'https://stepik.org/lesson/236904/step/1',
+    'https://stepik.org/lesson/236905/step/1',
 ]
 
 @pytest.mark.parametrize('url', URLS)
-def test_guest_should_see_login_link(browser, url):
+def test_parametrization(browser, url):
     link = f"{url}"
     browser.get(link)
 
@@ -34,5 +41,13 @@ def test_guest_should_see_login_link(browser, url):
             EC.element_to_be_clickable((By.CSS_SELECTOR, ".smart-hints__hint"))
         )
     assert hint.text == "Correct!", "The answer is wrong"
+
+    incorrect_hints = ''
+    if hint.text != "Correct!":
+        incorrect_hints += hint.text
     print(hint.text)
 
+    try:
+        assert 'Correct!' == hint.text
+    except AssertionError:
+        incorrect_hints += hint.text  # собираем ответ про Сов с каждой ошибкой
