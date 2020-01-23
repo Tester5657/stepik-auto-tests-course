38d39e9e3bfa0afad6649d1b9a92212386810d3b
kazantsev-sergey <bbmm@bk.ru>
2020-01-23 10:34:27 +0700

    added chapter3 lesson 8 - options for parametrization

--- chapter_3/lesson6_step8_parametrization/conftest.py
+++ chapter_3/lesson6_step8_parametrization/conftest.py
@@ -1,11 +1,14 @@
 import pytest
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="session")
 def browser():
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(options=options)
+    #browser = webdriver.Chrome()
     yield browser
     print("\nquit browser..")
     browser.quit()
--- chapter_3/lesson6_step8_parametrization/lesson6_step8_parametrization.py
+++ chapter_3/lesson6_step8_parametrization/lesson6_step8_parametrization.py
@@ -9,8 +9,8 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
-@pytest.mark.parametrize('language', ["ru", "en-gb"])
-def test_guest_should_see_login_link(browser, language):
-    link = f"http://selenium1py.pythonanywhere.com/{language}/"
+
+def test_guest_should_see_login_link(browser):
+    link = f"http://selenium1py.pythonanywhere.com/"
     browser.get(link)
     browser.find_element_by_css_selector("#login_link")
