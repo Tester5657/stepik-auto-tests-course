85816bc552307c5e48a8c8b5113757e19085ec1d
kazantsev-sergey <bbmm@bk.ru>
2020-01-29 18:01:06 +0700

    added chapter3 lesson 8 - options for parametrization

--- chapter_3/lesson6_step8_parametrization/lesson6_step8_parametrization.py
+++ chapter_3/lesson6_step8_parametrization/lesson6_step8_parametrization.py
@@ -1,16 +1,14 @@
 import pytest
 from selenium import webdriver
-
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
-
+import time
 
 def test_guest_should_see_login_link(browser):
     link = f"http://selenium1py.pythonanywhere.com/"
     browser.get(link)
     browser.find_element_by_css_selector("#login_link")
+    time.sleep(4)
+
+    """
+    pytest -s -v --language=fr chapter_3/lesson6_step8_parametrization/lesson6_step8_parametrization.py 
+
+    """
